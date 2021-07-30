9dc0ee7e615923056d049b7a35689a7d8120ad8e
8kengo <mi191324@edu.okinawa-ct.ac.jp>
2021-07-30 11:12:15 +0900

    プロジェクト作成

---
+++ app/src/main/java/mi191324/example/stareffort/MainActivity.kt
@@ -0,0 +1,11 @@
+package mi191324.example.stareffort
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
