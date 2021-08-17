7736c59fa0b7b867e8cc7e54bf110a84edacf9c6
tapiokamai10315858@gmail.com <tapiokamai10315858@gmail.com>
2021-08-17 18:27:04 +0900

    メイン画面からボタンタップで任意の画面に遷移するようにした

--- app/src/main/java/mi191324/example/stareffort/MainActivity.kt
+++ app/src/main/java/mi191324/example/stareffort/MainActivity.kt
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 
+
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
