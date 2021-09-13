0c6faae488fdb288bb9e5f09372e0c9cee952150
tapiokamai10315858@gmail.com <tapiokamai10315858@gmail.com>
2021-09-13 19:20:23 +0900

    MPAndroidChartを使う為の行の追加

--- app/src/main/java/mi191324/example/stareffort/FriendrecordActivity.kt
+++ app/src/main/java/mi191324/example/stareffort/FriendrecordActivity.kt
@@ -1,5 +1,6 @@
 package mi191324.example.stareffort
 
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
@@ -10,26 +11,37 @@ import com.github.mikephil.charting.components.YAxis
 import com.github.mikephil.charting.utils.ColorTemplate
 import com.github.mikephil.charting.data.*
 import kotlinx.android.synthetic.main.activity_friendrecord.*
+import kotlinx.android.synthetic.main.activity_friendrecord.barChart
+import kotlinx.android.synthetic.main.activity_myrecord.*
 
 class FriendrecordActivity : AppCompatActivity() {
     lateinit var barList: ArrayList<BarEntry>
+
+    lateinit var barDataSet: BarDataSet
+    lateinit var barData: BarData
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_friendrecord)
         val btnBack :Button = findViewById(R.id.btnBack)
 
-        //３）戻るボタン（アクティビティの終了）
+        //戻るボタン（アクティビティの終了）
         btnBack.setOnClickListener {
             finish()
         }
         barList = ArrayList()
-        barList.add(BarEntry(10f, 500f))
-        barList.add(BarEntry(10f, 500f))
-        barList.add(BarEntry(10f, 500f))
-        barList.add(BarEntry(10f, 500f))
-        barList.add(BarEntry(10f, 500f))
-        barList.add(BarEntry(10f, 500f))
-        barList.add(BarEntry(10f, 500f))
+        barList.add(BarEntry(1f, 500f))
+        barList.add(BarEntry(2f, 100f))
+        barList.add(BarEntry(3f, 300f))
+        barList.add(BarEntry(4f, 800f))
+        barList.add(BarEntry(5f, 400f))
+        barList.add(BarEntry(6f, 1000f))
+        barList.add(BarEntry(7f, 800f))
+        barDataSet = BarDataSet(barList, "Population")
+        barData = BarData(barDataSet)
+        barChart.data = barData
+        barDataSet.setColors(ColorTemplate.JOYFUL_COLORS, 250)
+        barDataSet.valueTextColor = Color.BLACK
+        barDataSet.valueTextSize = 15f
     }
 }
