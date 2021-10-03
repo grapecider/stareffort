c8d4f961fc9959530c31532dc6bd682c8c6c8eb3
8kengo <mi191324@edu.okinawa-ct.ac.jp>
2021-10-04 00:11:29 +0900

    自身の名前変更をサーバーに反映させる

--- app/src/main/java/mi191324/example/stareffort/profileActivity.kt
+++ app/src/main/java/mi191324/example/stareffort/profileActivity.kt
@@ -12,7 +12,12 @@ import android.widget.Toast
 import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import android.preference.PreferenceManager
+import com.github.kittinunf.fuel.httpPost
+import com.google.gson.Gson
+import com.squareup.moshi.KotlinJsonAdapterFactory
+import com.squareup.moshi.Moshi
 import org.w3c.dom.Text
+import java.util.HashMap
 
 class profileActivity : AppCompatActivity(){
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -40,11 +45,38 @@ class profileActivity : AppCompatActivity(){
             dialog.setTitle("新しい文字を入力してください")
             dialog.setView(myedit)
             dialog.setPositiveButton("OK", DialogInterface.OnClickListener {_, _ ->
-                val newname = myedit.getText().toString()
-                Toast.makeText(this, "名前を変更しました", Toast.LENGTH_SHORT).show()
+                val httpurl = "https://asia-northeast1-iconic-exchange-326112.cloudfunctions.net/name_change"
+                val moshi = Moshi.Builder().add(KotlinJsonAdapterFactory()).build()
+                val requestAdapter = moshi.adapter(userlist::class.java)
+                val header: HashMap<String, String> = hashMapOf("Content-Type" to "application/json")
+                var newname:String = myedit.getText().toString()
+                if (newname.length == 0){
+                    newname = username!!
+                }
+                val pushtext = userlist(id = myid!!, name = newname)
+                val httpAsync = (httpurl)
+                    .httpPost()
+                    .header(header).body(requestAdapter.toJson(pushtext))
+                    .responseString { request, response, result ->
+                        Log.d("hoge", result.toString())
+                        when(result){
+                            is com.github.kittinunf.result.Result.Failure ->{
+                                val ex = result.getException()
+                                Log.d("response", ex.toString())
+                                val myToast: Toast = Toast.makeText(this, "送信失敗しました", Toast.LENGTH_LONG)
+                                myToast.show()
+                            }
+
+                            is com.github.kittinunf.result.Result.Success -> {
+                                val data = result.get()
+                                Log.d("responce", data)
+                                Toast.makeText(this, "名前を変更しました", Toast.LENGTH_SHORT).show()
+                            }
+                        }
+                    }
+
                 edit.putString("username", newname)
                     .apply()
-
                 Log.d("newname", newname)
                 finish()
                 val intent = Intent(this, profileActivity::class.java)
@@ -58,4 +90,9 @@ class profileActivity : AppCompatActivity(){
         idtxt.setText(idset)
         nametxt.setText(username)
     }
+
+    data class userlist (
+        val id: String,
+        val name: String
+    )
 }
