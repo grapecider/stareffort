4b08c7212a5c2a36d6fd3c787c514333ea0feb32
8kengo <mi191324@edu.okinawa-ct.ac.jp>
2021-10-21 14:19:45 +0900

    通知許可画面のボタンの状態維持

--- app/src/main/java/mi191324/example/stareffort/notificationActivity.kt
+++ app/src/main/java/mi191324/example/stareffort/notificationActivity.kt
@@ -1,7 +1,10 @@
 package mi191324.example.stareffort
 
+import android.content.Context
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
+import android.widget.ToggleButton
 import androidx.appcompat.app.AppCompatActivity
 
 class notificationActivity : AppCompatActivity() {
@@ -10,7 +13,27 @@ class notificationActivity : AppCompatActivity() {
         setContentView(R.layout.activity_notification)
 
         val btnBack : Button = findViewById(R.id.returnBtn)
+        val notifiBtn: ToggleButton = findViewById(R.id.notifiBtn)
+        val shardPreferences = getSharedPreferences("KEY", Context.MODE_PRIVATE)
+        val edit = shardPreferences.edit()
+        val btnstate = shardPreferences.getString("btnstate", "0")
 
+        if (btnstate == "1") {
+            notifiBtn.isChecked = true
+        }
+
+        //通知許可ボタンタップ時の処理
+        notifiBtn.setOnCheckedChangeListener { _, isChecked ->
+            if (isChecked) {
+                Log.d("button", "OK")
+                edit.putString("btnstate", "1")
+                    .apply()
+            } else {
+                Log.d("button", "NO")
+                edit.putString("btnstate", "0")
+                    .apply()
+            }
+        }
         //戻るボタン（アクティビティの終了）
         btnBack.setOnClickListener {
             finish()
