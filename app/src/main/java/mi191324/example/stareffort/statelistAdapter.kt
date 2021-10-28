ea46a777ebb965340550669bde6ee132e5bf5f4f
8kengo <mi191324@edu.okinawa-ct.ac.jp>
2021-10-28 15:54:09 +0900

    MainActivityのレイアウト変更

--- app/src/main/java/mi191324/example/stareffort/statelistAdapter.kt
+++ app/src/main/java/mi191324/example/stareffort/statelistAdapter.kt
@@ -6,6 +6,7 @@ import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.core.content.ContextCompat.startActivity
 import androidx.recyclerview.widget.RecyclerView
@@ -27,11 +28,13 @@ class statelistAdapter(private val stateList: List<MainActivity.statelist>): Rec
         val state = currentItem.state
         if (state == 0){
             holder.textView3.text = "オフライン"
+            holder.imageView.setImageResource(R.drawable.ofline)
         }
         else {
-            holder.textView3.text = "オンライン中"
+            holder.textView3.text = "勉強中"
+            holder.imageView.setImageResource(R.drawable.online)
         }
-        holder.textView2.text = currentItem.id
+        holder.textView2.text = "@" + currentItem.id
         Log.d("adapter_", currentItem.id)
         Log.d("adapter_", currentItem.user)
         Log.d("adapter_", currentItem.state.toString())
@@ -50,5 +53,6 @@ class statelistAdapter(private val stateList: List<MainActivity.statelist>): Rec
         val textView1: TextView = itemView.findViewById(R.id.text1)
         val textView2: TextView = itemView.findViewById(R.id.text2)
         val textView3: TextView = itemView.findViewById(R.id.text3)
+        val imageView: ImageView = itemView.findViewById(R.id.imageView)
     }
 }
